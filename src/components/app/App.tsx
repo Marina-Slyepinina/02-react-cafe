6dbbdd5761d1a4f1e59867c24cd04326371f692c
Marina <marina.slepinina3@gmail.com>
2025-05-18 01:22:43 +0300

    update App

---
+++ src/components/app/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import css from "./App.module.css";
+import type { Votes, VoteType } from "../../types/votes";
+import CafeInfo from "../cafeInfo/CafeInfo";
+import VoteOptions from "../voteOptions/VoteOptions";
+import VoteStats from "../voteStatus/VoteStatus";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>(
+    {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    }    
+  )
+
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1
+    })
+  }
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    })
+  }
+  
+  const totalVotes: number = votes.good + votes.neutral + votes.bad;
+  const positiveRate: number = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0
+
+  return (
+    <>
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+        <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate}/>
+      </div>
+    </>
+  )
+}
+
